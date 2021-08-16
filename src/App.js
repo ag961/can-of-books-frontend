84e85f9d44952d1f141e3d2b29812b1926ca2ac8
Ayrat <gimranov45@gmail.com>
2021-08-16 08:29:00 +0300

    finished lab11

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import Login from './Login';
+import { withAuth0 } from '@auth0/auth0-react';
+import BestBooks from './BestBooks';
+import Profile from './Profile';
+
 
 class App extends React.Component {
 
   render() {
-    console.log('app', this.props);
-    return(
+    console.log('app', this.props.auth0);
+    const { user, isAuthenticated } = this.props.auth0;
+    console.log(user);
+    return (
       <>
         <Router>
           <IsLoadingAndError>
             <Header />
             <Switch>
               <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {isAuthenticated ? <BestBooks /> : <Login />}
               </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
             </Switch>
             <Footer />
           </IsLoadingAndError>
@@ -31,4 +40,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0(App);
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,18 +2,42 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
 class MyFavoriteBooks extends React.Component {
+
+  makeRequest = async () => {
+
+    const { getIdTokenClaims } = this.props.auth0;
+    let tokenClaims = await getIdTokenClaims();
+    const jwt = tokenClaims.__raw;
+    console.log(jwt);
+    const config = {
+      headers: { "Authorization": `Bearer ${jwt}` },
+    };
+
+    const serverResponse = await axios.get(`${process.env.REACT_APP_SERVER}/test`, config);
+    console.log('it worked', serverResponse);
+  }
+
   render() {
-    return(
+    const { user } = this.props.auth0;
+
+
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
+        {user ?
+          <button onClick={this.makeRequest}>Make Request to Server</button>
+          : ''
+        }
       </Jumbotron>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
